2eee6cb4fb840004fe7dc76ea998f6147819a5ce
Daniel Khan <daniel@khan.io>
2024-02-18 16:44:50 +0100

    fixes

--- python-service-gateway/app.py
+++ python-service-gateway/app.py
@@ -18,22 +18,20 @@ def index():
     choice = request.args.get('choice', '')
     url = f"http://localhost:{'3010' if toggle < 3 else '3020'}?choice={choice}"
 
-    # Simulate slowdown on ~30% of the requests
-    if random.random() < 0.3:
-        # fibonacci(35)  # This will cause a deliberate slowdown
-        try:
-            response = requests.get(url)
-            # Increment or reset the toggle based on its current value
-            toggle = toggle + 1 if toggle < 3 else 0
-
-            if response.status_code > 299:
-                # If there is an error with the request, respond with the error
-                return jsonify(error='Something went wrong!'), response.status_code
-            # Return the JSON response from the request
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            # Handle connection errors
-            return jsonify(error=str(e)), 500
+    try:
+        if random.random() < 0.3:
+            fibonacci(35)  # This will cause a deliberate slowdown
+        response = requests.get(url)
+        # Increment or reset the toggle based on its current value
+        toggle = toggle + 1 if toggle < 3 else 0
+        if response.status_code > 299:
+            # If there is an error with the request, respond with the error
+            return jsonify(error='Something went wrong!'), response.status_code
+        # Return the JSON response from the request
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        # Handle connection errors
+        return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
